c389ec3c102b003d259d4603a2e92fa5213070a0
rose <rose@roses-MacBook-Pro.local>
2026-06-04 14:32:27 -0400

    Add Github OIDC deployment and Github Action workflow

--- terraform_files/outputs.tf
+++ terraform_files/outputs.tf
@@ -1,3 +1,18 @@
 output "cloudfront_url" {
   value = aws_cloudfront_distribution.website.domain_name
 }
+
+output "cloudfront_distribution_id" {
+  description = "CloudFront Distribution ID"
+  value       = aws_cloudfront_distribution.website.id
+}
+
+output "cloudfront_distribution_domain_name" {
+  description = "CloudFront Domain Name"
+  value       = aws_cloudfront_distribution.website.domain_name
+}
+
+output "bucket_name" {
+  description = "Website bucket"
+  value       = aws_s3_bucket.website.bucket
+}
